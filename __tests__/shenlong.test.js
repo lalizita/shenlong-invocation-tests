54d59daa03c2a108172efe7dfd1d58b69b382419
Laís Lima <lais@cosmobots.io>
2019-10-24 18:18:57 -0300

    teste finalizado

--- __tests__/shenlong.test.js
+++ __tests__/shenlong.test.js
@@ -19,4 +19,16 @@ describe("Shenlong test", () => {
     Shenlong.changeDragonBallsList(['4 estrelas', '1 estrela', '6 estrelas'])
     expect(Shenlong.getDragonBallsList()).toEqual(expected)
   })
+  it("Should not invoke Shenlong without 7 dragon balls", () => {
+    expect(Shenlong.invokeShenlong()).toEqual("Você precisa ter as 7 esferas para invocar o Shenlong")
+  })
+  it("Should not invoke shenlong without magic words", () => {
+    Shenlong.changeDragonBallsList(['1 estrela', '2 estrelas', '3 estrelas', '4 estrelas', '5 estrelas', '6 estrelas', '7 estrelas'])
+    expect(Shenlong.invokeShenlong()).toEqual("Você precisa dizer as palavras mágicas")
+  })
+  it("Should not invoke shenlong", () => {
+    const magicWords = "Apareça Shenlong e realize o meu desejo"
+    Shenlong.changeDragonBallsList(['1 estrela', '2 estrelas', '3 estrelas', '4 estrelas', '5 estrelas', '6 estrelas', '7 estrelas'])
+    expect(Shenlong.invokeShenlong(magicWords)).toEqual("Você tem direito a 3 pedidos")
+  })
 })
--- main.js
+++ main.js
@@ -26,6 +26,14 @@ class Shenlong{
   clearDragonBallList(){
     this.dragonBalls = []
   }
+
+  invokeShenlong(magicWords){
+    const magic = "Apareça Shenlong e realize o meu desejo"
+    if(this.dragonBalls < 7) return 'Você precisa ter as 7 esferas para invocar o Shenlong'
+    if(!magicWords || magicWords !== magic) return 'Você precisa dizer as palavras mágicas'
+    this.isVisible = true
+    return "Você tem direito a 3 pedidos"
+  }
 }
 
 module.exports.Shenlong = new Shenlong
